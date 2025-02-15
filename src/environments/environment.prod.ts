2fc286fb23cf558610ffa15b6288f42777df9716
Njb <sdr.expert@gmail.com>
2025-02-15 21:11:10 +0200

    build-3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   auth: {
-    apiUrl: 'http://localhost:3101/api/',
-  },
+    email: 'admin-bot-1x',
+    password: 'tJfB59iWxAXL',
+    apiUrl: 'http://localhost:3101/api/'
+  }
 };
