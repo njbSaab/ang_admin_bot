3bc047463f544a629fb3ef78f05b3313f8873298
Njb <sdr.expert@gmail.com>
2025-02-19 02:33:17 +0200

    added routings

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'add-button', component: AddButtonComponent },
   { path: 'push', component: PushComponent },
   { path: 'users', component: UsersComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
